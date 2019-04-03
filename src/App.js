286abbcd3d5d8d69be9492daca88cebcd99be99e
jonatay <jono.stretch@gmail.com>
2019-04-03 22:15:28 +0200

    added Header\

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-
+import Header from './component/header';
+import './app.scss'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h2>Test</h2>
-        </header>
+        <Header/>
       </div>
     );
   }
